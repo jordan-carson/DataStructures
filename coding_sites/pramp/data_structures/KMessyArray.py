6e99f864844af400b9c43c99b8686d0d2b65d3e1
jordan-carson <jordanlouiscarson@gmail.com>
2019-08-10 09:12:07 -0400

    Adding K Messy array solution -> pramp

--- coding_sites/pramp/data_structures/KMessyArray.py
+++ coding_sites/pramp/data_structures/KMessyArray.py
@@ -1,5 +1,27 @@
+import heapq
 
 
-al = [1, 2, 3, 4]
+def sort_k(arr, k):
 
-al.pop(5)
+    # create a list of the first k + 1 elements
+    heap = arr[:k + 1]
+
+    heapq.heapify(heap)
+
+    target_index = 0
+    for i in range(k + 1, len(arr)):
+        arr[target_index] = heapq.heappop(heap)
+        heapq.heappush(heap, arr[i])
+        target_index += 1
+
+    while heap:
+        arr[target_index] = heapq.heappop(heap)
+        target_index += 1
+
+    return arr
+
+
+
+k = 3
+arr = [2, 6, 3, 12, 56, 8]
+print(sort_k(arr, k))
