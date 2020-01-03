ccedda3dd74928e8ce4d7396f120d10f10a5b9a7
jordancarson <jordanlouiscarson@gmail.com>
2020-01-02 22:35:10 -0500

    uploading LinkedList practice problems

--- coding_sites/pramp/data_structures/GettingADifferentNumberEfficient.py
+++
@@ -1,9 +0,0 @@
-def get_different_number(arr):
-    largest = max(arr)
-    out = [None for _ in range(largest+1)]
-    for a in arr:
-        out[a] = a
-    for i in range(len(out)):
-        if out[i] is None:
-            return i
-    return len(out)
